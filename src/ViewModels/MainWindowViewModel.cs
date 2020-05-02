1adfa4b6947699e73f53cce887da1325ca77da19
Dan Walmsley <dan@walms.co.uk>
2020-05-02 17:08:34 -0300

    load albums from hard coded folder.

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -9,16 +9,86 @@ using System.Threading.Tasks;
 using ReactiveUI;
 using SharpAudio;
 using SharpAudio.Codec;
+using Id3;
+using Avalonia.Media.Imaging;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace Symphony.ViewModels
 {
+    public static class TagExtensions
+    {
+        public static IBitmap LoadAlbumCover(this Id3Tag tag)
+        {
+            var cover = tag.Pictures.FirstOrDefault(x => x.PictureType == Id3.Frames.PictureType.FrontCover);
+
+            if (cover != null)
+            {
+                using (var ms = new MemoryStream(cover.PictureData))
+                {
+                    try
+                    {
+                        return new Bitmap(ms);
+                    }
+                    catch (Exception)
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+    }
+
+
+    public class Track
+    {
+        public string Title { get; set; }
+
+        public Album Album { get; set; }
+
+        public string Path { get; set; }
+    }
+
+    public class Album : IComparable<Album>
+    {
+        public Album()
+        {
+            Tracks = new List<Track>();
+        }
+
+        public string Title { get; set; }
+
+        public List<Track> Tracks { get; set; }
+
+        public IBitmap Bitmap { get; set; }
+
+        public int CompareTo([AllowNull] Album other)
+        {
+            return Title.CompareTo(other.Title);
+        }
+    }
+
     public class MainWindowViewModel : ViewModelBase
     {
         private TrackStatusViewModel _trackStatus;
+        private Dictionary<string, Album> _albums;
+        private ObservableCollection<IBitmap> _albumCovers;
+
+        public ObservableCollection<IBitmap> AlbumCovers
+        {
+            get { return _albumCovers; }
+            set { this.RaiseAndSetIfChanged(ref _albumCovers, value); }
+        }
 
         public MainWindowViewModel()
         {
+            _albums = new Dictionary<string, Album>();
             TrackStatus = new TrackStatusViewModel();
+            AlbumCovers = new ObservableCollection<IBitmap>();
 
             this._audioEngine = AudioEngine.CreateDefault();
 
@@ -31,12 +101,57 @@ namespace Symphony.ViewModels
                  .DistinctUntilChanged()
                 .Subscribe(x => this.TargetFile = x);
 
-            TargetFile = @"C:\Users\danwa\OneDrive\Music\Music\Elton John\Greatest Hits (1970-2002) [3CD]\01 your song.mp3";
+            //TargetFile = @"C:\Users\danwa\OneDrive\Music\Music\Elton John\Greatest Hits (1970-2002) [3CD]\01 your song.mp3";
+            TargetFile = @"C:\Users\danwa\OneDrive\Music\Music\Arctic Monkeys\Arctic Monkeys  Essentials\1-15 Black Treacle.mp3";
 
             PlayCommand = ReactiveCommand.CreateFromTask(DoPlay);
+
+            ScanMusicFolder(@"C:\Users\danwa\OneDrive\Music\Music\");
         }
 
         public ReactiveCommand<Unit, Unit> PlayCommand { get; }
+
+        public async Task ScanMusicFolder(string path)
+        {
+            foreach (var directory in Directory.EnumerateDirectories(path))
+            {
+                await ScanMusicFolder(directory);
+            }
+
+            foreach (var file in Directory.EnumerateFiles(path, "*.mp3"))
+            {
+                Console.WriteLine($"Processing file: {file}");
+
+                using (var mp3 = new Mp3(file))
+                {
+                    var tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                    if (tag is null)
+                    {
+                        continue;
+                    }
+
+                    if (!_albums.ContainsKey(tag.Album))
+                    {
+                        var album = new Album();
+
+                        album.Title = tag.Album;
+                        album.Tracks.Add(new Track
+                        {
+                            Album = album,
+                            Path = file,
+                            Title = tag.Title
+                        });
+
+                        album.Bitmap = tag.LoadAlbumCover();
+
+                        _albums[tag.Album] = album;
+
+                        AlbumCovers.Add(album.Bitmap);
+                    }
+                }
+            }
+        }
 
         public async Task DoPlay()
         {
