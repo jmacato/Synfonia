96516c03f6213296cf18f7f7081a2fc7e562bad8
Jumar Macato <hikari.netto23@gmail.com>
2020-05-02 17:00:43 +0800

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Symphony.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
