1fcec10947044ebfb377279a920121215aede057
Dan Walmsley <dan@walms.co.uk>
2020-05-02 13:05:46 -0300

    add controls for layout.

---
+++ src/Views/TrackStatusView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Symphony.Views
+{
+    public class TrackStatusView : UserControl
+    {
+        public TrackStatusView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/Views/VolumeControlView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Symphony.Views
+{
+    public class VolumeControlView : UserControl
+    {
+        public VolumeControlView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
