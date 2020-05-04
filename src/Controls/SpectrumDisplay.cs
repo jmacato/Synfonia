3ec7733bc168484450577a8d2256ed9bbe147297
Dan Walmsley <dan@walms.co.uk>
2020-05-04 14:27:07 -0300

    smooth spectrum

--- src/Controls/SpectrumDisplay.cs
+++ src/Controls/SpectrumDisplay.cs
@@ -3,38 +3,63 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media;
 using Avalonia.Threading;
+using ReactiveUI;
+using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
 
 namespace Symphony.Controls
 {
     public class SpectrumDisplay : UserControl
     {
-        IPen LinePen = new Pen(new SolidColorBrush(Colors.LightGray, 0.5), 1);
+        private IPen _linePen = new Pen(new SolidColorBrush(Colors.LightGray, 0.5), 1);
+        private double _lastStrokeThickness;
+        private double[] _averagedData;
+        private int _averageLevel = 10;
 
         public override void Render(DrawingContext context)
         {
             base.Render(context);
 
+            context.DrawRectangle(new Pen(new SolidColorBrush(Colors.Red, 0.5)), new Rect(0, 0, Bounds.Width, Bounds.Height));
+
             if (FFTData != null)
             {
+                if (_averagedData is null || FFTData.Length != _averagedData.Length)
+                {
+                    _averagedData = new double[FFTData.Length];
+                }
+
                 for (int i = 0; i < FFTData.Length; i++)
                 {
-                    context.DrawLine(LinePen, new Point(i, Bounds.Height), new Point(i, Bounds.Height * (1 - FFTData[i])));
-                    FFTData[i] *= 0.9;
+                    _averagedData[i] -= _averagedData[i] / _averageLevel;
+                    _averagedData[i] += FFTData[i] / _averageLevel;
+                }
+
+                var gaps = FFTData.Length - 1;
+
+                var gapSize = 2;
+                if ((gaps * gapSize) > Bounds.Width)
+                {
+                    gapSize = 0;
+                }
+
+                var binStroke = (Bounds.Width - (gaps * gapSize)) / FFTData.Length;
+
+                if (_lastStrokeThickness != binStroke)
+                {
+                    _lastStrokeThickness = binStroke;
+                    _linePen = new Pen(new SolidColorBrush(Colors.LightGray, 0.5), _lastStrokeThickness);
+                }
+
+                double x = binStroke / 2;
+                for (int i = 0; i < _averagedData.Length; i++)
+                {
+                    context.DrawLine(_linePen, new Point(x, Bounds.Height), new Point(x, Bounds.Height * (1 - _averagedData[i])));
+                    x += (binStroke + 2);
                 }
 
                 Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Background);
-            }
-        }
-
-        protected override Size MeasureOverride(Size availableSize)
-        {
-            if (FFTData != null)
-            {
-                return new Size(FFTData.Length, availableSize.Height);
-            }
-            else
-            {
-                return new Size(1, availableSize.Height);
             }
         }
 
@@ -55,6 +80,20 @@ namespace Symphony.Controls
         static SpectrumDisplay()
         {
             AffectsMeasure<SpectrumDisplay>(FFTDataProperty);
+        }
+
+        public SpectrumDisplay()
+        {
+            this.GetObservable(FFTDataProperty)
+                .Throttle(TimeSpan.FromMilliseconds(250))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x =>
+                {
+                    if (FFTData != null)
+                    {
+                        FFTData = new double[FFTData.Length];
+                    }
+                });
         }
     }
 }
