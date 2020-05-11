ba5a47bb4e16880f2176095af1acef42ea967fe2
Dan Walmsley <dan@walms.co.uk>
2020-05-10 21:52:38 -0300

    implement drag and drop album to trackstatus area.

---
+++ src/Views/AlbumView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using System;
+
+namespace Synfonia.Views
+{
+    public class AlbumView : UserControl
+    {
+        public AlbumView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
