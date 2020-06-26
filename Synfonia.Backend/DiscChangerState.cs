d9745b7e2c53d9186870e7ef438815d640680b7d
Jumar Macato <hikari.netto23@gmail.com>
2020-06-26 23:36:24 +0800

    simplify states

--- Synfonia.Backend/DiscChangerState.cs
+++ Synfonia.Backend/DiscChangerState.cs
@@ -3,10 +3,7 @@ namespace Synfonia.Backend
     public enum DiscChangerState
     {
         Idle,
-        LoadingTrack,
         Playing,
-        CmdTriggered,
-        CurrentTrackDone,
         Paused,
     }
 }
