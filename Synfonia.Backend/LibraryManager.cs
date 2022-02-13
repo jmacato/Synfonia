b13a2ea7b352bb842e8e644eef4d662db476e55e
Dan Walmsley <dan@walms.co.uk>
2022-02-13 19:29:18 +0000

    dont support flac for now.

--- Synfonia.Backend/LibraryManager.cs
+++ Synfonia.Backend/LibraryManager.cs
@@ -15,7 +15,7 @@ namespace Synfonia.Backend
         private static readonly List<string> SupportedFileExtensions = new List<string>
         {
             "3ga", "669", "a52", "aac", "ac3", "adt", "adts", "aif", "aifc", "aiff",
-            "amb", "amr", "aob", "ape", "au", "awb", "caf", "dts", "dsf", "dff", "flac", "it", "kar",
+            "amb", "amr", "aob", "ape", "au", "awb", "caf", "dts", "dsf", "dff", "it", "kar",
             "m4a", "m4b", "m4p", "m5p", "mka", "mlp", "mod", "mpa", "mp1", "mp2", "mp3", "mpc", "mpga", "mus",
             "oga", "ogg", "oma", "opus", "qcp", "ra", "rmi", "s3m", "sid", "spx", "tak", "thd", "tta",
             "voc", "vqf", "w64", "wav", "wma", "wv", "xa", "xm"
