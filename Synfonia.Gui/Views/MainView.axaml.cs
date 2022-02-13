954337da698c9503beb8bd22533a74c20db467ce
Dan Walmsley <dan@walms.co.uk>
2022-02-13 16:40:03 +0000

    add new layout.

---
+++ Synfonia.Gui/Views/MainView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Synfonia.Gui.Views;
+
+public class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
