b0251d82010d9805d0438f077d4440741e2946db
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-02-15 20:34:24 +0800

    Force to use Ffmpeg all the time for now
    Should be reverted once we get ffmpeg working on windows.

--- SharpAudio/SharpAudio.Codec/SoundStream.cs
+++ SharpAudio/SharpAudio.Codec/SoundStream.cs
@@ -54,36 +54,14 @@ namespace SharpAudio.Codec
             }
             else
             {
-                var fourcc = stream.ReadFourCc();
-                stream.Seek(0, SeekOrigin.Begin);
-
-                if (fourcc.SequenceEqual(MakeFourCC("RIFF")))
-                {
-                    _decoder = new WaveDecoder(stream);
-                    IsStreamed = false;
-                }
-                else if (fourcc.SequenceEqual(MakeFourCC("ID3\u0001")) ||
-                         fourcc.SequenceEqual(MakeFourCC("ID3\u0002")) ||
-                         fourcc.SequenceEqual(MakeFourCC("ID3\u0003")) ||
-                         fourcc.AsSpan(0, 2).SequenceEqual(new byte[] { 0xFF, 0xFB }))
-                {
-                    _decoder = new Mp3Decoder(stream);
-                    IsStreamed = true;
-                }
-                else if (fourcc.SequenceEqual(MakeFourCC("OggS")))
-                {
-                    _decoder = new VorbisDecoder(stream);
-                    IsStreamed = true;
-                }
-                else
-                {
-                    _decoder = new FFmpegDecoder(stream);
-                    IsStreamed = true;
-                }
+                throw new InvalidOperationException("Cannot support playing Stream objects at this time.");
             }
 
-            _streamThread = new Thread(MainLoop);
-            _streamThread.Name = "SoundStream";
+            _streamThread = new Thread(MainLoop)
+            {
+                Name = "SoundStream"
+            };
+            
             _streamThread.Start();
         }
 
@@ -239,14 +217,6 @@ namespace SharpAudio.Codec
         public void Stop()
         {
             State = SoundStreamState.Stop;
-        }
-
-        private static byte[] MakeFourCC(string magic)
-        {
-            return new[] {  (byte)magic[0],
-                (byte)magic[1],
-                (byte)magic[2],
-                (byte)magic[3]};
         }
     }
 }
